1845993ca84ff93c68994ab05a64f2ee1ad7994a
Amit Singh <69297309+AmitSinghChauhanDEV@users.noreply.github.com>
2021-10-06 20:10:28 +0530

    Update diarypage.php

--- diarypage.php
+++ diarypage.php
@@ -25,11 +25,11 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/dstyle.css">
+    <link rel="stylesheet" href="dstyle.css">
 
     <title>My Diary</title>
   </head>
-  <body background="img/1.jpg">
+  <body background="1.jpg">
 
   <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
 		  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -53,8 +53,8 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
 
-    <script src="js/jquery.js"></script>
-    <script src="js/action.js"></script>
+    <script src="jquery.js"></script>
+    <script src="action.js"></script>
    
   </body>
 </html>
